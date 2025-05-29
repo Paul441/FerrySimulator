c57a37048dc38ce004b4e00ff58147f2fca8b035
Paul441 <pawelpienkowski0@gmail.com>
2025-05-29 09:05:21 +0200

    Update Config.java

--- Config.java
+++ Config.java
@@ -2,7 +2,7 @@ package com.example.ferry;
 
 import java.util.List;
 
-/** Odwzorowanie struktury YAML → obiekt. */
+
 public class Config {
     public int dockCapacity;
     public int carArrivalIntervalMs;
