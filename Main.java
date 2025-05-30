808c63c193a56ad3f9fbf48c2848b5c68cc8893f
Paul441 <pawelpienkowski0@gmail.com>
2025-05-30 18:56:23 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package com.example.ferry;
 
 public class Main {
     public static void main(String[] args) {
-       //ekran startowy
+       
         javax.swing.SwingUtilities.invokeLater(Launcher::new);
     }
 }
